dbeaf414ad0237a775622f1f34fc32fca04a95e7
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-04 18:16:23 +0200

    list all page in admin panel

--- resources/views/allpage.blade.php
+++ resources/views/allpage.blade.php
@@ -1,3 +1,13 @@
-@foreach ($pages as $page)
-    Page title: {{$page->page_title}}
-@endforeach
+<a href="{{url('admin')}}">Go back</a>
+<table border="1">
+    <tr><th>Page id</th><th>Page title</th><th>Page body</th><th>Edit</th><th>Delete</th></tr>
+    @foreach ($pages as $page)
+        <tr>
+            <td>{{$page->page_id}}</td>
+            <td>{{$page->page_title}}</td>
+            <td>{{$page->page_body}}</td>
+            <td>Edit</td>
+            <td>Delete</td>
+        </tr>
+    @endforeach
+</table>
