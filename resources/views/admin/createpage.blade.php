c851f1a51905bc7ebd51f0b2ca85008baf920fd2
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-05 16:08:54 +0200

    form display old value 2

--- resources/views/admin/createpage.blade.php
+++ resources/views/admin/createpage.blade.php
@@ -16,8 +16,8 @@
 <form method="post" action="{{url('admin/page/create')}}">
     @csrf
     @method('post')
-    <label>Title<br><input type="text" name="title" style="width: 200px"></label><br>
-    <label>Body<br><textarea name="body" style="width: 200px"></textarea></label><br>
+    <label>Title<br><input type="text" name="title" style="width: 200px" value="{{old('title')}}"></label><br>
+    <label>Body<br><textarea name="body" style="width: 200px">{{old('body')}}</textarea></label><br>
     <input type="submit"/>
 </form>
 @endsection
