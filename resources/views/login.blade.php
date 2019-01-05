44aa05fd934d488d6c7293929748999fdc40e98e
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-05 16:07:13 +0200

    form display old value 1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,7 +16,7 @@
     @csrf
     @method('POST')
 
-    <label>Username<input name="username" type="text"/></label>
+    <label>Username<input name="username" type="text" value="{{old('username')}}"/></label>
     <label>Password<input name="password" type="password"/></label>
 
     <input type="submit"/>
