a76757d36328c542116a37c405e323549f85d1ed
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-07 16:53:08 +0200

    Display title of page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,15 +1,15 @@
 @extends('layout.layout')
-@section('title', 'Page')
+@section('title',"{$page_title}")
 @section('content')
     <div>
-    <nav style="float: right;">
-        <p>Navigation</p>
-        @foreach($pages as $page)
-            <p><a href="{{url('page/'.$page->page_id)}}">{{$page->page_title}}</a></p>
-        @endforeach
-        <p>--------</p>
-        <a href="{{url('/admin')}}">Admin</a>
-    </nav>
+        <nav style="float: right;">
+            <p>Navigation</p>
+            @foreach($pages as $page)
+                <p><a href="{{url('page/'.$page->page_id)}}">{{$page->page_title}}</a></p>
+            @endforeach
+            <p>--------</p>
+            <a href="{{url('/admin')}}">Admin</a>
+        </nav>
     </div>
     <h1>{{$page_title}}</h1>
     <div>{{$page_body}}</div>
