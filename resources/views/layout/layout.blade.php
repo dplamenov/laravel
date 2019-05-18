4753c3015c2faca312cacac04dd35747ccdbaaa0
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-18 10:49:28 +0300

    import ckeditor

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -2,6 +2,7 @@
 <head>
     <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}" />
+    <script src="{{url('js/ckeditor/ckeditor.js')}}"></script>
 </head>
 <body>
 <div class="container">
