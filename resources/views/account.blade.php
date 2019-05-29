652e0fdef3da327c717ab178e0b00b360ce9946d
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-29 16:06:26 +0300

    import bootstarp

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -4,8 +4,9 @@
     <a href="{{url('admin')}}">Go back</a>
     <h1>Account settings</h1>
     <form>
-        <input type="text" name="username"/>
-        <input type="password" name="password">
+        <label>Username<input type="text" name="username" style="margin-bottom: 2px;margin-left: 10px"/></label>
+        <br>
+        <label>Password<input type="password" name="password" style="margin-bottom: 2px;margin-left: 10px"></label>
     </form>
 @endsection
 
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -2,6 +2,7 @@
 <head>
     <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="{{url('js/ckeditor/ckeditor.js')}}"></script>
     @section('script')
         @show
