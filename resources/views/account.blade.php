a17ae9032459ad6988a103ce855ed365b933598d
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-29 16:02:32 +0300

    account view go back link

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.layout')
 @section('title', 'Account')
 @section('content')
+    <a href="{{url('admin')}}">Go back</a>
     <h1>Account settings</h1>
     <form>
         <input type="text" name="username"/>
